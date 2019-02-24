56b167aeb62da81059e3678afa01c9c5fe291316
ameliecomte <comamelie@gmail.com>
2019-02-24 07:00:50 +0100

    fundamental structure of Character class

---
+++ model/Character.php
@@ -0,0 +1,54 @@
+<?php
+class Character
+{
+    private $name;
+    private $health;
+    private $level;
+    private $strength;
+
+    public function __construct()
+    {
+
+    }
+
+
+    public function setName()
+    {
+
+    }
+
+    public function setHealth()
+    {
+
+    }
+
+    public function setLevel()
+    {
+
+    }
+
+    public function setStrength()
+    {
+
+    }
+
+    public function getName()
+    {
+
+    }
+
+    public function getHealth()
+    {
+
+    }
+
+    public function getLevel()
+    {
+
+    }
+
+    public function getStrength()
+    {
+
+    }
+}
